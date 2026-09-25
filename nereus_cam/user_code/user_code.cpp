79258ef19b827eeb58d0fcfb13a055f65d4976ae
Matthew Krause <kraus2mj@gmail.com>
2026-09-25 10:44:17 -0700

    Do not limit the number of commands

--- nereus_cam/user_code/user_code.cpp
+++ nereus_cam/user_code/user_code.cpp
@@ -12,10 +12,7 @@
 #define NUMBER_OF_COMMANDS_QUEUED 10
 #define COMMAND_WAIT_KEY "cmdWaitMs"
 
-static BmTimer timer[NUMBER_OF_COMMANDS_QUEUED] = {NULL};
 static uint32_t uptime_wait_ms = 60000;
-static uint8_t idx = 0;
-static BmSemaphore mut = NULL;
 static bool released = false;
 
 typedef struct {
@@ -46,9 +43,7 @@ static void timer_cb(void *arg) {
   }
 
   free_info(info);
-  bm_semaphore_take(mut, BM_MAX_DELAY_UINT32);
   bm_timer_delete(t, 0);
-  bm_semaphore_give(mut);
 }
 
 static void sub_cb(uint64_t, const char *topic, uint16_t topic_len,
@@ -58,14 +53,6 @@ static void sub_cb(uint64_t, const char *topic, uint16_t topic_len,
 
   // If uptime is greater than the wait threshold do nothing
   if (released || uptime_ms >= uptime_wait_ms) {
-    return;
-  }
-
-  bm_semaphore_take(mut, BM_MAX_DELAY_UINT32);
-  // Commands are full
-  if (idx >= array_size(timer)) {
-    bm_semaphore_give(mut);
-    bm_debug("Number of commands full in command queue...");
     return;
   }
 
@@ -85,22 +72,18 @@ static void sub_cb(uint64_t, const char *topic, uint16_t topic_len,
   memcpy(info->topic, topic, topic_len);
   memcpy(info->data, data, data_len);
 
-  timer[idx] = bm_timer_create("cmd_hold", trigger_ms, false, info, timer_cb);
-  if (timer[idx] && bm_timer_start(timer[idx], 0) == BmOK) {
-    idx++;
-  } else {
-    bm_debug("Could not create timer...");
+  BmTimer timer =
+      bm_timer_create("cmd_hold", trigger_ms, false, info, timer_cb);
+  if (!timer || bm_timer_start(timer, 0) != BmOK) {
+    bm_debug("Could not start timer...");
     free_info(info);
   }
-
-  bm_semaphore_give(mut);
 }
 
 void setup() {
   serial_bridge_init();
   get_config_uint(BM_CFG_PARTITION_SYSTEM, COMMAND_WAIT_KEY,
                   sizeof(COMMAND_WAIT_KEY), &uptime_wait_ms);
-  mut = bm_mutex_create();
   bm_sub("bmcam/cmd", sub_cb);
   IOWrite(&BB_VBUS_EN, 0);
   // ensure Vbus stable before enable Vout with a 5ms delay.
